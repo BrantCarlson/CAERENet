53d3fd293fcdd2a3051f882a7d61bd4aa98b93f1
Brant Carlson <bcarlson1@carthage.edu>
2014-06-01 13:43:21 +0200

    adding configuration file

---
+++ conf.py
@@ -0,0 +1,13 @@
+"""
+Configuration local to the user's computer goes here, to avoid constant conflicts in other files.
+
+i.e. data directory locations are here.  Edit this file to your local location.
+
+If you don't want to update this file every time you synchronize your code, do this:
+    git update-index --assume-unchanged conf.py
+
+if you want git to update it,
+    git update-index --no-assume-unchanged conf.py
+"""
+
+dataDir = "/home/brant/caerenet/data/"
