0840f42472e995e266c3712640b93bc6a925c089
Brant Carlson <bcarlson1@carthage.edu>
2014-08-12 16:58:17 -0500

    adding pseudoSPI draft (probably buggy)
    untested

---
+++ rpi/ccode/pseudoSPI.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define MCLK 0
+#define SCLK 1
+#define MOSI 2
+#define MISO 3
+
+void setup(){
+  if(wiringPiSetup() == -1){
+    printf("wiringPiSetup failed...?\n");
+    exit(1);
+  }
+
+  pinMode(MCLK, OUTPUT);
+  pinMode(SCLK, INPUT);
+  pinMode(MOSI, OUTPUT);
+  pinMode(MISO, INPUT);
+}
+
+void shutdown(){
+  pinMode(MCLK, INPUT);
+  pinMode(SCLK, INPUT);
+  pinMode(MOSI, INPUT);
+  pinMode(MISO, INPUT);
+}
+
+int io1(int send){
+  int in;
+
+  digitalWrite(MCLK,1);
+
+  while(digitalRead(SCLK)==0){
+    continue; // add a delay here?
+    //delay(1); // 1 ms delay
+  }
+
+  digitalWrite(MOSI,send);
+  digitalWrite(MCLK,0);
+
+  while(digitalRead(SCLK)==1){
+    continue; // add a delay here?
+    //delay(1); // 1 ms delay
+  }
+
+  in = digitalRead(MISO);
+}
+
+int main(void){
+  int x;
+  setup();
+
+  while(true){
+    printf("sending 1...\n");
+    x = io1(1);
+    printf("sent 1, got %d.\n",x);
+
+    printf("sending 0...\n");
+    x = io1(0);
+    printf("sent 0, got %d.\n",x);
+
+  }
+
+  shutdown();
+
+  return 0;
+}
