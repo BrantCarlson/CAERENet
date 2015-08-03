85604052908d51c3391f7de463314e17e6654877
Brant Carlson <bcarlson1@carthage.edu>
2015-08-03 09:05:40 -0500

    working on julia stuff, bigger box gcode

--- dataProc/readdata.jl
+++ dataProc/readdata.jl
@@ -4,7 +4,8 @@ using DataFrames
 
 const bufsize=1024
 const clkfreq = 80.0e6
-const nsampPerChanPerBuf = div((bufsize-4),3)
+const nchan = 3
+const nsampPerChanPerBuf = div((bufsize-4),nchan)
 
 function readData(fn)
   nbytes = stat(fn).size
@@ -26,21 +27,41 @@ function readData(fn)
     end
   end
 
-  
-  t0 = zeros(nbufs*nsampPerChanPerBuf)
-  t1 = zeros(nbufs*nsampPerChanPerBuf)
-  t2 = zeros(nbufs*nsampPerChanPerBuf)
-  for i=1:nbufs*nsampPerChanPerBuf
-    t0[i] = 
-    t1[i] =
-    t2[i] = 
+  t0 = zeros(nsampPerChanPerBuf,nbufs)
+  t1 = zeros(nsampPerChanPerBuf,nbufs)
+  t2 = zeros(nsampPerChanPerBuf,nbufs)
+  for i=1:nbufs
+    for j=1:nsampPerChanPerBuf
+      dt2 = (te[i]-ts[i])/(nsampPerChanPerBuf*nchan+1)
+      dt1 = 3*dt2
+
+      t0[j,i] = ts[i] + j*dt1
+      t1[j,i] = ts[i] + j*dt1 + dt2
+      t2[j,i] = ts[i] + j*dt1 + 2*dt2
+    end
   end
 
   c0 = reshape(a[3:3:end-2,:],nbufs*nsampPerChanPerBuf)
+  t0 = reshape(t0,nbufs*nsampPerChanPerBuf)
   c1 = reshape(a[4:3:end-2,:],nbufs*nsampPerChanPerBuf)
+  t1 = reshape(t0,nbufs*nsampPerChanPerBuf)
   c2 = reshape(a[5:3:end-2,:],nbufs*nsampPerChanPerBuf)
+  t2 = reshape(t0,nbufs*nsampPerChanPerBuf)
 
-  DataFrame(c0=c0,c1=c1,c2=c2)
+  DataFrame(t0=t0,c0=c0,t1=t1,c1=c1,t2=t2,c2=c2)
+end
+
+function procData(a,pgthr=1000)
+  # generate new data frame
+  # extract basic statistics using photogate-chunked filtered data:
+  # - max minus min
+  # - variance
+  # - peak slope
+
+  
+
+  pgt = 
+
 end
 
 end #endmodule
